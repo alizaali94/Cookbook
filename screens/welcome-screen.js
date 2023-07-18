61cc86e55edd96ce34511bdc47bd55b27e900d4b
AlizaAli9490 <115713076+AlizaAli9490@users.noreply.github.com>
2023-07-18 17:43:23 +0500

    Deploying the Cooking Recipies Project to GITHub as Version 1

---
+++ screens/welcome-screen.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import Colors from './config/colors';
+
+
+const  WelcomeScreen = () => {
+  return (
+    <ImageBackground source={require('./assets/backgroundImage.jpeg')} style= {{flex:1}}>
+    <View style = {{flex:1, backgroundColor : "#00000050"}}>
+      <View style = {{
+          position : 'absolute',
+          height : '100%',
+          zIndex : 2,
+          width: "100%",
+          justifyContent: 'flex-end',
+          paddingHorizontal : 20,
+          paddingBottom: 40
+      }}>
+        <View>
+          <Text style = {{ color: Colors.white, fontWeight: '800', fontSize : 35, textTransform: 'capitalize'}}>
+            Let's your Favourite Food find You
+          </Text>
+          <Text style = {{ color: Colors.white ,fontWeight: '600', fontSize : 15}}>
+          Embark on a Delicious Journey in the World of Cooking
+          </Text>
+          <TouchableOpacity style= {{
+            padding: 15,  
+            width: 150, 
+              backgroundColor: Colors.white,
+              borderRadius: 50,
+              alignItems: "center",
+              marginTop: 15}}>
+            <Text style={{
+               color: Colors.black,
+               fontSize: 15,
+               fontWeight: "700",
+            }}>Explore Now</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
